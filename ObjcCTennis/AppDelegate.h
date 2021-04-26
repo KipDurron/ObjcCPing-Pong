238f280df4e3986a098540ca86d2a68b6a08a35f
kipdurron <ishunin97@gmail.com>
2021-04-26 10:43:21 +0300

    Initial Commit

---
+++ ObjcCTennis/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ObjcCTennis
+//
+//  Created by Илья Кадыров on 26.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ObjcCTennis/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ObjcCTennis
+//
+//  Created by Илья Кадыров on 26.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
